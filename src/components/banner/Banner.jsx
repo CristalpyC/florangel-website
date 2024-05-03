3f6f5b68381e1bca653a4a6e4d5b5a3ceea76d39
cristalPyC <cristaltn03@gmail.com>
2024-05-03 18:41:47 -0400

    Fixing bugs

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -13,9 +13,9 @@ export const Banner = () => {
         </div>
         
         <img className="florangel-pic" src="/florangel-img-2.png" alt="Florangel" />
-        <div className="florangel-pic2">
+        {/*<div className="florangel-pic2">
           <img src="/florangel-img-2.png" alt="Florangel" />
-        </div>
+  </div>*/}
        {/* <div className="wave"></div>*/}
       </div>
       <img className="wavy" src="/wave.svg" alt="" />
